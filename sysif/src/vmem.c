79a436eea7d48acaa71d785a3744947cc8a46063
koromodako <koromodako@gmail.com>
2015-12-02 07:57:39 +0100

    Ajout vmem et commentaires kheap

--- sysif/src/vmem.c
+++ sysif/src/vmem.c
@@ -1,1 +1,16 @@
 #include "vmem.h"
+#include "kheap.h"
+
+unsigned int init_kern_translation_table(void)
+{
+	//uint32_t device_flags = 0b010000110111;
+
+	//uint8_t * table1_addr = (uint8_t*)(kAlloc_aligned(FIRST_LVL_TT_SIZE, 0));
+
+	//int page; 
+	//for(page = 0; page < 0; ++page)
+	//{
+
+	//}
+	return 0;
+}
--- sysif/src/vmem.h
+++ sysif/src/vmem.h
@@ -1,15 +1,24 @@
 #ifndef VMEM_H
 #define VMEM_H
 
-
 // Macros ----------------------------------------------------------------------
 #define PAGE_SIZE 4000  // En Octet = 4Ko
 
-#define FIRST_LVL_TT_COUN 4096 // (soit 2^12, 12 bits pour l'adresse niveau 1) Nombre d'entrées niveau 1
+#define FIRST_LVL_TT_COUN 4096 // (soit 2^12, 12 bits pour une entrée du niveau 1) Nombre d'entrées dans la table de niveau 1
 #define FIRST_LVL_TT_SIZE 16384 // (Nombre d'entrées x 4) En Octet
 
 #define SECON_LVL_TT_COUN 256 // Nombre d'entrées niveau 2
 #define SECON_LVL_TT_SIZE 1024 // (Nombre d'entrées x 4) En Octet
+
+
+// Fonctions ------------------------------------------
+
+// Memory initialization -----------
+unsigned int init_kern_translation_table(void);
+
+// Memory management ---------------
+void alloc_page(void);
+void free_page(void);
 
 
 #endif //VMEM_H
