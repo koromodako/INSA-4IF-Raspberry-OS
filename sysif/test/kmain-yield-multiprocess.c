c2e4c555d6b45f6d09d6939416db6dc582023282
Antoine Chabert <chabert.antoine@gmail.com>
2015-12-13 01:03:25 +0100

    Correction du test 12 : TODO trouver pourquoi l'aumgentation des itérations fait planter le test !

--- sysif/test/kmain-yield-multiprocess.c
+++ sysif/test/kmain-yield-multiprocess.c
@@ -31,12 +31,12 @@ void kmain(void) {
     create_process((func_t*)&user_process_1, PP_MEDIUM);
     create_process((func_t*)&user_process_2, PP_MEDIUM);
     create_process((func_t*)&user_process_3, PP_MEDIUM);
-    
+
     timer_init();
     ENABLE_IRQ();
-    __asm("cps 0x10");
-    // switch CPU to USER mode
-    // **********************************************************************
+
+    SWITCH_TO_USER_MODE;
+
     while (1) {
         sys_yield();
     }
