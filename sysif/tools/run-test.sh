32f2720ed203cd676d2a48adaff4923928317ba5
Antoine Chabert <chabert.antoine@gmail.com>
2015-12-13 01:02:38 +0100

    Suppression des warnings et du mode graphic de qemu lors des tests

--- sysif/tools/run-test.sh
+++ sysif/tools/run-test.sh
@@ -7,12 +7,12 @@ SCRIPTFILE=$(realpath $2)
 kill_qemu() { killall -9 qemu-system-arm > /dev/null 2>&1 ;}
 
 cd $TOOLSDIR
-cd .. ; make -B KMAIN=$SOURCEFILE || exit 1
+cd .. ; make -B KMAIN=$SOURCEFILE > /dev/null 2>&1 || exit 1
 
 cd $TOOLSDIR
 
 kill_qemu
-./run-qemu.sh > /dev/null 2>&1 &
+./run-qemu.sh -nographic > /dev/null 2>&1 &
 
 ./run-gdb.sh $SCRIPTFILE
 
