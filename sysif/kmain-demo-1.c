48bd69fa5de1c6ef8d75fbdcbe3ec7127efa0bdf
Antoine Chabert <chabert.antoine@gmail.com>
2016-01-11 18:43:06 +0100

    Mise en place de la demo 1

--- sysif/kmain-demo-1.c
+++ sysif/kmain-demo-1.c
@@ -14,102 +14,53 @@
 #include "syscall.h"
 #include "drivers/usb/csud/include/device/hid/keyboard.h"
 
-static FontTable * font;
+extern unsigned char _binary_img_1_ppm_start;
+extern unsigned char _binary_img_1_ppm_end;
+extern unsigned char _binary_img_1_pgm_start;
+extern unsigned char _binary_img_1_pgm_end;
 
-extern unsigned char _binary_img_lena_ppm_start;
-extern unsigned char _binary_img_lena_ppm_end;
-extern unsigned char _binary_img_lena_pgm_start;
-extern unsigned char _binary_img_lena_pgm_end;
-extern unsigned char _binary_img_landscape_ppm_start;
-extern unsigned char _binary_img_landscape_ppm_end;
-extern unsigned char _binary_img_landscape_pgm_start;
-extern unsigned char _binary_img_landscape_pgm_end;
+extern unsigned char _binary_img_2_ppm_start;
+extern unsigned char _binary_img_2_ppm_end;
+extern unsigned char _binary_img_2_pgm_start;
+extern unsigned char _binary_img_2_pgm_end;
 
-void display_process_top_info() {
+extern unsigned char _binary_img_3_ppm_start;
+extern unsigned char _binary_img_3_ppm_end;
+extern unsigned char _binary_img_3_pgm_start;
+extern unsigned char _binary_img_3_pgm_end;
 
-    // Affichage text haut gauche
-    FontCursor * cursor = initCursor(10, 10, getResolutionX(), getResolutionY());
-    drawLetters(cursor, font, "Hello World !\n");
-    char * resolutionX = (char *) kAlloc(sizeof (char) * 12);
-    char * resolutionY = (char *) kAlloc(sizeof (char) * 12);
-    itoa(getResolutionX() + 1, resolutionX);
-    itoa(getResolutionY() + 1, resolutionY);
-    drawLetters(cursor, font, "Resolution : ");
-    drawLetters(cursor, font, resolutionX);
-    drawLetters(cursor, font, "x");
-    drawLetters(cursor, font, resolutionY);
-    drawLetters(cursor, font, "\n");
-
-    // Affichage séparateurs
-    drawLine(10, 70, getResolutionX() - 10, 70); // Horizontal : Entre les infos du haut et le reste
-    drawLine(divide32(getResolutionX(), 2), 10, divide32(getResolutionX(), 2), 60); // Vertical : Entre les infos du haut (milieu)
-    drawLine(divide32(getResolutionX(), 2), 80, divide32(getResolutionX(), 2), getResolutionY() - 10); // Vertical : Milieu du bas
-    drawLine(divide32(getResolutionX(), 2) + 10, divide32(getResolutionY(), 2), getResolutionX() - 10, divide32(getResolutionY(), 2)); // Horizontal : Milieu de la droite
-
-    // Affichage infos clavier
-    FontCursor * cursorClavierTime = initCursor(divide32(getResolutionX(), 2) + 10, 10, getResolutionX() - 10, 60);
-    char * nbClavierString = (char *) kAlloc(sizeof (char) * 14);
-    uint32_t nbClavier = getNbKeyboard();
-    itoa(nbClavier, nbClavierString);
-    drawLetters(cursorClavierTime, font, "Nb de claviers : ");
-    drawLetters(cursorClavierTime, font, nbClavierString);
-    drawLetters(cursorClavierTime, font, "\n");
-}
-
-void display_process_right_top_text() {
-    FontCursor * cursorRight = initCursor(divide32(getResolutionX(), 2) + 10, 90, getResolutionX() - 10, divide32(getResolutionY(), 2) - 10);
-
-    uint32_t letter = 33;
-    while (1) {
-         drawLetter(cursorRight, font, (char) letter);
-         letter++;
-         if (letter > 126) {
-             letter = 33;
-         }
-         uint32_t sleep = 0;
-         for (sleep = 0; sleep < 100000; sleep++);
-    }
-}
-
-void display_process_right_bottom_keyboard() {
-    FontCursor * cursorLeft = initCursor(divide32(getResolutionX(), 2) + 10, divide32(getResolutionY(), 2) + 10, getResolutionX() - 10, getResolutionY() - 10);
-    drawLetters(cursorLeft, font, "Raspberry-OS:~$ ");
-    if (getNbKeyboard() > 0) {
-        for (;;) {
-            KeyboardUpdate();
-            char c = KeyboardGetChar();
-            if (c != 0) {
-                drawLetter(cursorLeft, font, c);
-            }
-        }
-    }
-}
+extern unsigned char _binary_img_4_ppm_start;
+extern unsigned char _binary_img_4_ppm_end;
+extern unsigned char _binary_img_4_pgm_start;
+extern unsigned char _binary_img_4_pgm_end;
 
 void display_process_left_image() {
     Image img[] = {
-            loadImage(PPM, &_binary_img_lena_pgm_start, &_binary_img_lena_pgm_end),
-            loadImage(PPM, &_binary_img_lena_ppm_start, &_binary_img_lena_ppm_end),
-            loadImage(PPM, &_binary_img_landscape_pgm_start, &_binary_img_landscape_pgm_end),
-            loadImage(PPM, &_binary_img_landscape_ppm_start, &_binary_img_landscape_ppm_end)
+            loadImage(PPM, &_binary_img_1_pgm_start, &_binary_img_1_pgm_end),
+            loadImage(PPM, &_binary_img_2_pgm_start, &_binary_img_2_pgm_end),
+            loadImage(PPM, &_binary_img_3_pgm_start, &_binary_img_3_pgm_end),
+            loadImage(PPM, &_binary_img_4_pgm_start, &_binary_img_4_pgm_end),
+
+            loadImage(PPM, &_binary_img_1_ppm_start, &_binary_img_1_ppm_end),
+            loadImage(PPM, &_binary_img_2_ppm_start, &_binary_img_2_ppm_end),
+            loadImage(PPM, &_binary_img_3_ppm_start, &_binary_img_3_ppm_end),
+            loadImage(PPM, &_binary_img_4_ppm_start, &_binary_img_4_ppm_end)
     };
 
     for (;;) {
-        for (uint8_t i=0; i<4; ++i) {
-            draw(10, 90, divide32(getResolutionX(), 2) - 5, getResolutionY() - 5, 0, 0, 0);
-            displayImage(img[i], 10, 90, divide32(getResolutionX(), 2) - 10, getResolutionY() - 10);
-            uint32_t sleep = 0;
-            for (sleep = 0; sleep < 1000000; sleep++);
+        for (uint8_t i=0; i<8; ++i) {
+            draw(0, 0, getResolutionX(), getResolutionY(), 0, 0, 0);
+            displayImage(img[i], 5, 5, getResolutionX() - 5, getResolutionY() - 5);
+            uint64_t sleep = 0;
+            for (sleep = 0; sleep < 300000000; sleep++);
         }
     }
 }
 
 void kmain(void) {
 
-    // Initialisation de l'USB
-    usb_init();
-
     // Initialisation du scheduler
-    sched_init(SP_PRIORITY);
+    sched_init(SP_SIMPLE);
 
     // Initialisation des LEDs ...
     hw_init();
@@ -118,21 +69,10 @@ void kmain(void) {
     FramebufferInitialize();
 
     // Creation des processus
-    create_process((func_t*) & display_process_top_info, PP_HIGH);
     create_process((func_t*) & display_process_left_image, PP_HIGH);
-    create_process((func_t*) & display_process_right_top_text, PP_LOW);
-    create_process((func_t*) & display_process_right_bottom_keyboard, PP_MEDIUM);
-
-    // Initialisation du timer matériel pour les IRQ
-    timer_init();
-    ENABLE_IRQ();
 
     // switch CPU to USER mode
     SWITCH_TO_USER_MODE;
 
-    font = initFont();
-
-    while (1) {
-        sys_yield();
-    }
+    sys_yield();
 }
--- sysif/tools/init.gdb
+++ sysif/tools/init.gdb
@@ -8,18 +8,6 @@ target remote:1234
 set history filename ~/.gdb_history
 set history save
 
-b *reset_asm_handler
-b *after_kmain
-b kernel_panic
-
-b *swi_handler
-b *irq_handler
-
-b display_process_top_info
-b display_process_right_top_text
-b display_process_right_bottom_keyboard
-b display_process_left_image
-
 source utils.gdb
 
 continue
